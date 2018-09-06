f2a9133393fe4beb84a5ea2bdf7a2681e7f9b767
Lewis <lewis@amp.studio>
2018-09-06 16:45:38 +0100

    Cleaned up TwitterFeed code

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-require("dotenv").config({ path: "variables.env" });
-const express = require("express");
-const compression = require("compression");
-const bodyParser = require("body-parser");
-const cron = require("node-cron");
-const cors = require("cors");
-const app = express();
-const { logging } = require("./scripts/logging");
-const { postToSlack } = require("./scripts/slack");
-const twitter = require("./scripts/twitterFeed");
+require("dotenv").config({ path: "variables.env" })
+const express = require("express")
+const compression = require("compression")
+const bodyParser = require("body-parser")
+const cron = require("node-cron")
+const cors = require("cors")
+const app = express()
+const { logging } = require("./scripts/logging")
+const { postToSlack } = require("./scripts/slack")
+const twitter = require("./scripts/twitterFeed")
 
 
 /*------------------------------*/
@@ -15,32 +15,32 @@ const twitter = require("./scripts/twitterFeed");
 /*------------------------------*/
 
 // Logs each request to the console with timestamp, IP, POST data etc
-app.use(logging);
+app.use(logging)
 // Enable GZIP compression
-app.use(compression());
+app.use(compression())
 // Serve static files from the ./static folder
-app.use(express.static("static"));
+app.use(express.static("static"))
 // Parse POST data into a useable format
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-);
+)
 
 // Allow CORS from these domains
-const whitelist = ["https://amp.studio", "http://localhost:3000", "http://localhost:5000"];
+const whitelist = ["https://amp.studio", "http://localhost:3000", "http://localhost:5000"]
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS"))
     }
   },
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions))
 
 
 /*------------------------------*/
@@ -48,20 +48,21 @@ app.use(cors(corsOptions));
 /*------------------------------*/
 
 // Fetch new AMP Twitter posts when script first runs
-let twitterPosts = twitter.getPosts("ampstudiouk", 2);
-// Refresh AMP Twitter posts every 5 minutes via cron job
+let twitterPosts = twitter.getPosts()
+
+// Refresh AMP Twitter posts every 5 minutes
 cron.schedule("*/5 * * * *", () => {
-  const latestPosts = twitter.getPosts("ampstudiouk", 2);
-  if (latestPosts) twitterPosts = latestPosts;
-});
+  const latestPosts = twitter.getPosts()
+  if (latestPosts) twitterPosts = latestPosts
+})
 
 // Route responds with JSON of latest AMP Twitter posts
 app.get("/twitter", (req, res, next) => {
-  res.json(twitterPosts);
+  res.json(twitterPosts)
   if (req.headers["user-agent"].includes("Insights")) {
-    res.setHeader("Cache-Control", "public, max-age=604800");
+    res.setHeader("Cache-Control", "public, max-age=604800")
   }
-});
+})
 
 
 /*------------------------------*/
@@ -72,18 +73,15 @@ app.get("/twitter", (req, res, next) => {
 // Forwards message to our friendly slackbot 🤖
 app.post("/contact/landing-page", async (req, res, next) => {
   // Extract useful data into individual variables with ES6 destructuring
-  const { name, email, company, industry, message, web, title } = req.body;
+  const { name, email, company, industry, message, web, title } = req.body
 
-  let errors = {};
-  if (!name) errors.name = true;
-  if (!email) errors.email = true;
-  if (!message) errors.message = true;
-  if (web) errors.bot = true;
-  if (Object.keys(errors).length > 0) {
-    res.status(200);
-    res.json(errors);
-    return;
-  }
+  let errors = {}
+  if (!name) errors.name = true
+  if (!email) errors.email = true
+  if (!message) errors.message = true
+  if (web) errors.bot = true
+  if (Object.keys(errors).length > 0)
+    return res.json(errors)
 
   // Build the formatted Slack message, 
   let text = `
@@ -92,24 +90,22 @@ app.post("/contact/landing-page", async (req, res, next) => {
 *Company Name:* ${company}
 *Industry:* ${industry}
 *Message:* ${message}
-  `;
+  `
 
   // Add the page reference to the Slack message
   // Keeps track of where the enquiry came from
-  if (title) {
-    text = `*Offer Page:* ${title}` + text
-  }
+  if (title)
+    text += `\n*Offer Page:* ${title}` 
 
   // Async post to Slack
-  const slackMessage = await postToSlack({ text });
+  const slackMessage = await postToSlack({ text })
 
   // Check for errors posting to Slack
-  if (slackMessage.err) errors.slack = true;
+  if (slackMessage.err) errors.slack = true
 
   // Send the response back to the client
-  res.status(200);
-  res.json(errors);
-});
+  res.json(errors)
+})
 
 
 /*------------------------------*/
@@ -117,7 +113,7 @@ app.post("/contact/landing-page", async (req, res, next) => {
 /*------------------------------*/
 
 // Boot up the server
-app.set("port", process.env.PORT || 9001);
+app.set("port", process.env.PORT || 9001)
 const server = app.listen(app.get("port"), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+  console.log(`Express running → PORT ${server.address().port}`)
+})
--- scripts/twitterFeed.js
+++ scripts/twitterFeed.js
@@ -1,6 +1,72 @@
-const CryptoJS = require("crypto-js");
-const axios = require("axios");
-const btoa = require("btoa");
+const CryptoJS = require("crypto-js")
+const axios = require("axios")
+const btoa = require("btoa")
+
+exports.getPosts = function(user = process.env.twitter_user, postCount = 2) {
+  const baseURL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+  // These parameters are required for generating an OAuth2 signature.
+  // See https://developer.twitter.com/en/docs/basics/authentication/guides/creating-a-signature.html
+  const signingKey = `${process.env.oauth_consumer_secret}&${process.env.oauth_access_secret}`
+  const parameters = {
+    // The user who own the Twitter Developer App account
+    screen_name: user,
+    // Number of posts to fetch
+    count: postCount.toString(),
+    // Nonce must be a random 32 byte string
+    oauth_consumer_key: process.env.oauth_consumer_key,
+    oauth_token: process.env.oauth_access_token,
+    oauth_nonce: (Math.random() * 1e49).toString(36),
+    oauth_timestamp: Math.floor(Date.now() / 1000),
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_version: "1.0"
+  }
+
+  const sortedParameters = Object.keys(parameters)
+    // Create an array of the parameters (see above)
+    .map(key => [encodeURIComponent(key), encodeURIComponent(parameters[key])])
+    // Sort parameters alphabetically
+    .sort((a, b) => a[0] > b[0])
+    // Create query string from sorted parameters
+    .map((parameter, i) => `${i === 0 ? "?" : "&"}${parameter.join("=")}` )
+    .join("")
+
+  const signatureBaseString = 
+    `GET&${encodeURIComponent(baseURL)}&${encodeURIComponent(sortedParameters)}`
+
+  // Generate hashed URI
+  const signature = CryptoJS.HmacSHA1(signatureBaseString, signingKey).toString()
+  const signatureURI = encodeURIComponent(hexToBase64(signature))
+
+  // Sign URL with hash
+  const signedURL =
+    baseURL + "?" + sortedParameters + "&oauth_signature=" + signatureURI
+
+    
+    axios
+    .get(signedURL)
+    .then(res => {
+      const posts = {}
+      res.data.forEach((post, i) => {
+        const postData = {
+          created_at: post.created_at.split(" ").slice(1, 3).join(" "),
+          permalink: `https://www.twitter.com/AMPstudioUK/status/${post.id_str}`,
+          text: post.text
+        }
+        if (post.retweeted_status) {
+          postData.retweeted_status = post.retweeted_status
+        }
+        posts[i] = postData
+      })
+      console.log(posts)
+      return posts
+    })
+    .catch(err => {
+      console.log("Error fetching Twitter feed")
+      // console.log(err)
+      return null
+    })
+  }
 
 function hexToBase64(str) {
   return btoa(
@@ -12,97 +78,5 @@ function hexToBase64(str) {
         .replace(/ +$/, "")
         .split(" ")
     )
-  );
+  )
 }
-
-exports.getPosts = function(user = "lewisbooth", postCount = 10) {
-  var method = "GET";
-  var baseURL = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-
-  // These are needed for generating an authentication signature.
-  // See https://developer.twitter.com/en/docs/basics/authentication/guides/creating-a-signature.html
-  var oauth_consumer_secret = process.env.oauth_consumer_secret;
-  var oauth_access_secret = process.env.oauth_access_secret;
-  var signingKey = oauth_consumer_secret + "&" + oauth_access_secret;
-  var parameters = {
-    // 32 byte random string
-    oauth_nonce: (Math.random() * 1e49).toString(36),
-    oauth_timestamp: Math.floor(Date.now() / 1000),
-    oauth_consumer_key: process.env.oauth_consumer_key,
-    oauth_token: process.env.oauth_access_token,
-    oauth_signature_method: "HMAC-SHA1",
-    oauth_version: "1.0",
-    screen_name: user,
-    count: postCount.toString()
-  };
-
-  var sortedParameters = [];
-
-  // Create an array of the above paramenters
-  for (key in parameters) {
-    sortedParameters.push([
-      encodeURIComponent(key),
-      encodeURIComponent(parameters[key])
-    ]);
-  }
-
-  // Sort the parameter array alphabetically by key
-  sortedParameters.sort(function(a, b) {
-    return a[0] > b[0];
-  });
-
-  var parameterString = "";
-
-  // Build a URI string from the sorted parameters
-  for (i = 0; i < sortedParameters.length; i++) {
-    if (i > 0) parameterString += "&";
-    parameterString += sortedParameters[i].join("=");
-  }
-
-  var signatureBaseString =
-    method +
-    "&" +
-    encodeURIComponent(baseURL) +
-    "&" +
-    encodeURIComponent(parameterString);
-
-  var signature = CryptoJS.HmacSHA1(signatureBaseString, signingKey).toString();
-  var binarySignature = hexToBase64(signature);
-  var signatureURI = encodeURIComponent(binarySignature);
-
-  var signedURL =
-    baseURL + "?" + parameterString + "&oauth_signature=" + signatureURI;
-
-  var posts = {};
-
-  axios
-    .get(signedURL)
-    .then(res => {
-      res.data.forEach((post, i) => {
-        const created_at = post.created_at
-          .split(" ")
-          .slice(1, 3)
-          .join(" ");
-        const postData = {
-          created_at,
-          permalink: `https://www.twitter.com/AMPstudioUK/status/${
-            post.id_str
-          }`,
-          text: post.text
-        };
-        if (post.retweeted_status) {
-          postData.retweeted_status = post.retweeted_status;
-        }
-        posts[i] = postData;
-      });
-      console.log("Fetched new Twitter feeds");
-    })
-    .catch(err => {
-      console.log(err)
-      console.log("Error fetching Twitter feed");
-      // console.log(err);
-      return null;
-    });
-
-  return posts;
-};
